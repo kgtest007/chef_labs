def440dacc822f6620e399036e501d687d82d36e
root <root@dev-ops-chef.devopspubad1.japacops.oraclevcn.com>
2019-01-03 09:23:00 +0000

    added new feature for fetching details from node objects

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -16,8 +16,14 @@ package 'zip'
 
 
 file '/etc/motd' do
-  content 'This server belongs to K-Gohil Inc. Any un-authorise access to this box will be considered as a violation of Cyber Laws could result into legal actions.'
+  content "This server belongs to K-Gohil Inc. Any un-authorise access to this box will be considered as a violation of Cyber Laws could result into legal actions.
+        HOSTNAME 	: #{node['hostname']}
+        IPADDRESS 	: #{node['ipaddress']}
+        MEMORY		: #{node['memory']['total']}
+        CPU		: #{node['cpu']['0']['mhz']}
+"
   owner 'root'
   group 'root'
   mode  '0644'
+
 end
