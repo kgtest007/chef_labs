863c80d3463f738a4db480fec05ba8095ec44589
root <root@dev-ops-chef.devopspubad1.japacops.oraclevcn.com>
2019-01-07 02:27:38 +0000

    modified the recipe to incluide variables for generating more dynamic templates

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -17,5 +17,10 @@ package 'zip'
 
 template '/etc/motd' do
   source 'motd.erb'
+  variables({
+    :host_desc => 'CHEF TEST SERVER',
+    :chef_v => node['chef_packages']['chef']['version'],
+    :ohai_v => node['chef_packages']['ohai']['version'] 
+  })
   action :create
 end
